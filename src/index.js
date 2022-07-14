91e390cd62ce70d9bccd3c26c7566ef86e5fa7dc
Carlos Mejia <cmejia@equitel.com.co>
2022-07-13 23:21:48 -0500

    route configuration

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
+import indexRoutes from "./routes/index.js";
+
 const app = express();
 
+//Ejs Configuration
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.set("views", join(__dirname, 'views'));
+app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+//Routes
+app.use(indexRoutes);
 
+//Listening port
 app.listen(3000);
 console.log("Server is listening on port", 3000);
