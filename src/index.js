21562e03c97fda320a4c3a3d5c0f5dae1510e7d4
Carlos Mejia <cmejia@equitel.com.co>
2022-07-13 20:57:18 -0500

    ejs module configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import express from 'express';
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("views", join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.listen(3000);
-console.log('Server is listening on port', 3000);
+console.log("Server is listening on port", 3000);
