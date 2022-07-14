0d61d20badab25e526ed2aa539620904aa232a36
Carlos Mejia <cmejia@equitel.com.co>
2022-07-13 23:33:45 -0500

    implementing ejs module

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.render("index"));
-router.get("/about", (req, res) => res.render("about"));
-router.get("/contact", (req, res) => res.render("contact"));
+router.get("/", (req, res) => res.render("index", { title : 'First Web with Node'}));
+router.get("/about", (req, res) => res.render("about", { title : 'About Me'}));
+router.get("/contact", (req, res) => res.render("contact", { title : 'Contact Page'}));
 
 export default router;
